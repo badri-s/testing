c7382d0829e1a5da86109cabd8653ed0408bed1c
badri-s <56728283+badri-s@users.noreply.github.com>
2019-11-29 15:52:09 +0400

    message

--- app/src/main/java/com/example/testing/MoreActivity.kt
+++ app/src/main/java/com/example/testing/MoreActivity.kt
@@ -1,7 +1,10 @@
 package com.example.testing
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_more.*
 
 class MoreActivity : AppCompatActivity() {
 
@@ -9,5 +12,9 @@ class MoreActivity : AppCompatActivity() {
         supportActionBar!!.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_more)
+        lokation.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.ge/maps/@41.737191,44.825371,12.04z?hl=ka&authuser=0")))
+
+        }
     }
 }
